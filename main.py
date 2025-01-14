d9b0c31ab6bb8461397cfa8d22bd4ec5392edb7f
LizaZou <119621159+LizaZou@users.noreply.github.com>
2025-01-14 12:01:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import threading
 import time
+#on importe les différentes fonctions de chaque fichier pour les lancer sur différents threads
+from fichier1 import fonction1
+from fichier2 import fonction2
+
+# Créer les threads
+thread1 = threading.Thread(target=fonction1, args=("argument1",))
+thread2 = threading.Thread(target=fonction2, args=("argument2", "argument3"))
+
+#on lance un compteur pour évaluer le temps de la requête
+start = time.perf_counter()
+
+# Lancer les threads avec arguments
+thread1.start()
+thread2.start()
+
+# Attendre la fin des threads
+thread1.join()
+thread2.join()
+
+#on évalue le temps qui s'est écoulé
+finish = time.perf_counter()
+print(f'Finished in {round(finish-start, 2)} second(s)')
+
+
 
