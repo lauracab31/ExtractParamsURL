711c0ddc641be1f24467a3adc9b5a188bf7e8050
LizaZou <119621159+LizaZou@users.noreply.github.com>
2025-01-20 12:00:02 +0100

    Update_main2.py

--- main.py
+++ main.py
@@ -5,13 +5,27 @@ from webTraffic import web_traffic
 from hasDNSRecord import has_DNS_Record
 from ageOfDomain import age_of_domain
 import adress_bar_based
-import count_external_links
+from count_external_links import count_external_links
 import html_js
 
 
 # Créer les threads
-thread1 = threading.Thread(target=fonction1, args=("argument1",))
-thread2 = threading.Thread(target=fonction2, args=("argument2", "argument3"))
+thread1 = threading.Thread(target=web_traffic, args=("argument1",))
+thread2 = threading.Thread(target=has_DNS_Record, args=("argument2"))
+thread3 = threading.Thread(target=age_of_domain, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.est_adresse_ip, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.longueur_url, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.contient_arobase, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.contient_sous_domaine, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.has_favicon, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.contient_https, args=("argument2"))
+thread3 = threading.Thread(target=count_external_links, args=("argument2"))
+thread3 = threading.Thread(target=adress_bar_based.has_favicon, args=("argument2"))
+
+
+
+
+
 
 #on lance un compteur pour évaluer le temps de la requête
 start = time.perf_counter()
