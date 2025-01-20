073e29f89f8b11d2bdf9e311dab655dac7099b25
LizaZou <119621159+LizaZou@users.noreply.github.com>
2025-01-20 16:14:30 +0100

    Update main3.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-from threadsManager import process_url()
-
+from threadsManager import process_url
+import json
 # test en local
 #url = "http://example.com"
 #data = {"key": "value"}
 #data=process_url(url, data)
 
 if __name__ == "__main__":
-    try:
-        # Ajouter des URLs à la liste d'attente
-        #urls_from_orchestrator = [
-            #"http://example.com",
-            #"http://test.com",
-            #"http://sample.org"
-        #]
-        #for url in urls_from_orchestrator:
-            #add_url_to_queue(url)
-
-        # Traiter les URLs
-        process_url()
-
-        # Attendre que toutes les tâches soient terminées
-        #url_queue.join()
-
-    except Exception as e:
-        print(f"Erreur inattendue : {e}")
+    def execute_Encoder_code(url):
+        try:
+            # Traiter l'URL et obtenir les résultats
+            url_data = process_url(url)
+            
+            # Convertir les résultats en JSON
+            url_data_json = json.dumps(url_data, indent=4)  # Ajoute une mise en forme pour lecture facile
+            print(f"Résultats JSON : {url_data_json}")
+            
+            return url_data_json
+        
+        except Exception as e:
+            print(f"Erreur inattendue : {e}")
+            # Retourner un objet JSON d'erreur
+            return json.dumps({"error": f"Erreur inattendue : {e}"})
