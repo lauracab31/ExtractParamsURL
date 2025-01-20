72548f9fd5500dce102cc34c4f4dc9facd136d39
LizaZou <119621159+LizaZou@users.noreply.github.com>
2025-01-20 15:18:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from threadsManager import process_url()
 
+# test en local
+#url = "http://example.com"
+#data = {"key": "value"}
+#data=process_url(url, data)
+
+if __name__ == "__main__":
+    try:
+        # Ajouter des URLs à la liste d'attente
+        #urls_from_orchestrator = [
+            #"http://example.com",
+            #"http://test.com",
+            #"http://sample.org"
+        #]
+        #for url in urls_from_orchestrator:
+            #add_url_to_queue(url)
+
+        # Traiter les URLs
+        process_url()
+
+        # Attendre que toutes les tâches soient terminées
+        #url_queue.join()
+
+    except Exception as e:
+        print(f"Erreur inattendue : {e}")
